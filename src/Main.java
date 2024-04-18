8f50fdd06a5f214dec8cf9021c67d175f56965be
ferd1az <fdiaz@elpuig.xeill.net>
2024-04-18 20:59:16 +0200

    Se mueve todo a la clase TUI, se llama desde el main.

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int opcion;
-        do {
-            System.out.println("1.Nueva partida");
-            System.out.println("2.Cargar partida");
-            System.out.println("3.Configuracion");
-            System.out.println("4.Salir");
-
-
-            opcion = sc.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    System.out.println("Ha seleccionado: Nueva partida");
-                    break;
-                case 2:
-                    System.out.println("Ha seleccionado: Cargar partida");
-                    break;
-                case 3:
-                    System.out.println("Ha seleccionado :Configuracion");
-                    break;
-                case 4:
-                    System.out.println("Ha seleccionado: Salir");
-                    salir();
-                    break;
-                default:
-                    System.out.println("Opción no válida. Por favor, seleccione una opción del menú.");
-                    break;
-            }
-            System.out.println();
-        } while (true);
-    }
-
-    private static void salir() {
-        System.exit(0);
-    }
-    private static void nuevaPartida(){
-    }
-    private static void cargarPartida(){
-
-    }
-    private static void configuracion(){
-
+        TUI TUI = new TUI();
+        TUI.start();
     }
 }
--- src/TUI.java
+++ src/TUI.java
@@ -1,2 +1,57 @@
+import java.util.Scanner;
+
 public class TUI {
+    private Scanner sc;
+
+    public void texTUI() {
+        sc = new Scanner(System.in);
+    }
+    public void start() {
+        boolean inicio = true;
+        while (inicio){
+            System.out.println("1.Nueva partida");
+            System.out.println("2.Cargar partida");
+            System.out.println("3.Configuracion");
+            System.out.println("4.Salir");
+
+            int opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ha seleccionado: Nueva partida");
+                    nuevaPartida();
+                    break;
+                case 2:
+                    System.out.println("Ha seleccionado: Cargar partida");
+                    cargarPartida();
+                    break;
+                case 3:
+                    System.out.println("Ha seleccionado :Configuracion");
+                    configuracion();
+                    break;
+                case 4:
+                    System.out.println("Ha seleccionado: Salir");
+                    salir();
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción del menú.");
+                    break;
+            }
+            sc.close();
+        }
+    }
+    private static void salir() {
+        System.exit(0);
+    }
+    private static void nuevaPartida(){
+    }
+    private static void cargarPartida(){
+
+    }
+    private static void configuracion(){
+
+    }
+
 }
+
