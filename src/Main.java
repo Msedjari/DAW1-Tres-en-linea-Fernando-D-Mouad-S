ac7b8fa33a068c726c297e612d5ec859e5d26e40
ferd1az <fdiaz@elpuig.xeill.net>
2024-04-18 19:33:00 +0200

    Se añade un switch sin acabar.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola, Tres en linea proximamente...");
+        Scanner sc = new Scanner(System.in);
+        int entradaUsaurio = sc.nextInt();
+        switch (entradaUsaurio) {
+            case 1:
+                System.out.println("1.Nueva Prtida");
+                break;
+            case 2:
+                System.out.println("2.Cargar partida");
+                break;
+            case 3:
+                System.out.println("3.Configuracion");
+                break;
+            case 4:
+                System.out.println("4.Salir");
+            default:
+
+
+        }
     }
 }
