af3ce02ba1ac2e2d9ca4e9c8219a5476f3763a35
ferd1az <fdiaz@elpuig.xeill.net>
2024-04-22 18:43:20 +0200

    Se elimina el bucle while y se meuve el menu dentro del metodo lanzarMenu

--- src/TUI.java
+++ src/TUI.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TUI {
-    private Scanner sc;
+    private static Scanner sc;
 
     public TUI() {
         sc = new Scanner(System.in);
@@ -12,8 +12,26 @@ public class TUI {
     }
 
     public void start() {
-        boolean inicio = true;
-        while (inicio){
+       lanzarMenu();
+    }
+    private static void salir() {
+        System.out.println("Saliendo del programa. ¡Hasta luego!");
+        System.exit(0);
+    }
+    private static void nuevaPartida(){
+        System.out.println("Iniciando nueva partida...");
+    }
+
+    private static void cargarPartida(){
+        System.out.println("Cargando partida...");
+
+    }
+    private static void configuracion(){
+        System.out.println("Accediendo a la configuración...");
+    }
+    private static void lanzarMenu(){
+        // Esta funcion debe lanzar el menu y devolver la opcion escogida.
+        // Cambiar el nombre a uno que describa mejor que hace el programa.
             System.out.println("1.Nueva partida");
             System.out.println("2.Cargar partida");
             System.out.println("3.Configuracion");
@@ -43,27 +61,6 @@ public class TUI {
                     System.out.println("Opción no válida. Por favor, seleccione una opción del menú.");
                     break;
             }
-        }
-        sc.close();
-    }
-    private static void salir() {
-        System.out.println("Saliendo del programa. ¡Hasta luego!");
-        System.exit(0);
-    }
-    private static void nuevaPartida(){
-        System.out.println("Iniciando nueva partida...");
-    }
-
-    private static void cargarPartida(){
-        System.out.println("Cargando partida...");
-
-    }
-    private static void configuracion(){
-        System.out.println("Accediendo a la configuración...");
-    }
-    private static void lanzarMenu(){
-        // Esta funcion debe lanzar el menu y devolver la opcion escogida.
-        // Cambiar el nombre a uno que describa mejor que hace el programa.
     }
 
 
