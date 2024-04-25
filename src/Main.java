9ec8e71fc089702df15dc4cb9f8491e45b9b67bc
ferd1az <fdiaz@elpuig.xeill.net>
2024-04-25 19:50:26 +0200

    Se modifica el main para: + main(in args: string [*]): el punto de entrada de la aplicacion, tendra que crear las instancias TUI y Joc, y mostrar el menu. - novaPartida(): Gestiona todo lo que hara falta para iniciar una partida nueva. - carregarPartida(): Gestiona todo lo que hara falta para cargar una partida previamente guardada. - configuracio(): Muestra el menu de configuracion. - sortir(): Sale de la aplicacion.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-import java.util.Scanner;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Main {
     public static void main(String[] args) {
         TUI TUI = new TUI();
         TUI.start();
+
+    }
+    private novaPartida(){
+        throw new NotImplementedException();
+    }
+    private carregarPArtida(){
+        throw new NotImplementedException();
+    }
+    private configuracio(){
+        throw new NotImplementedException();
+    }
+    private sortir(){
+        throw new NotImplementedException();
     }
 }
