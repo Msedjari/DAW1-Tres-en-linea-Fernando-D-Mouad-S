e9ff2205ec538ed6a795618b5be95e09439c1f42
Fernando DG <fdiaz@elpuig.xeill.net>
2024-05-13 21:05:42 +0200

    Se importa el master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class Main {
     public static void main(String[] args) {
         TUI tui = new TUI();
-        boolean select = true
+        boolean select = true;
         do{
         int opcion = tui.mostrarMenuPrincipal();
             switch (opcion) {
